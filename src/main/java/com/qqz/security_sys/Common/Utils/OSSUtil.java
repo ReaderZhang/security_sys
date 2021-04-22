22fa07e99d55a0acc2b51ce77408aff04f37444d
ReaderZhang <50573019+ReaderZhang@users.noreply.github.com>
2021-04-23 01:14:24 +0800

    Update OSSUtil.java

--- src/main/java/com/qqz/security_sys/Common/Utils/OSSUtil.java
+++ src/main/java/com/qqz/security_sys/Common/Utils/OSSUtil.java
@@ -12,8 +12,8 @@ import java.util.Date;
 
 public class OSSUtil {
     public final static String endpoint = "https://oss-cn-beijing.aliyuncs.com";
-    public final static String accesskey = "LTAI4GJ5JjbF68bv6xz9x7fS";
-    public final static String secret = "cJQLuAuwWGcS7sZWFmBXj2X8pe5Hr1";
+    public final static String accesskey = "xxxxxxxxxxxxxxx";
+    public final static String secret = "xxxxxxxxxxxxxxxxxxxxxxx";
     public final static String bucketname = "oss-qqz-security-sys";
     public final static String domain = "https://oss-qqz-security-sys.oss-cn-beijing.aliyuncs.com/";
     public final static SimpleDateFormat sdf = new SimpleDateFormat ("yyyyMMddhhmmss");
